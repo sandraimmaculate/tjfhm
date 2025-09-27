a4b41a5cee2f6436a976d637d9340ec60d0718d7
sandraimmaculate <immaculate6281@gmail.com>
2025-09-27 16:19:55 +0530

    changed

--- codedeployment.sh
+++ codedeployment.sh
@@ -3,4 +3,4 @@ mkdir -p /home/jenkins/jesus
 cd /home/jenkins/jesus
 apt-get update 
 touch thankyoujesus.txt
-echo "jesus im sorry i litterally not knowing it is right or wrong im unable to control my self please dont punish vinay or my mom or my dad i will try to get rid from this sin please help to get rid from this sin enaku unarthunga im sorry appa please dont punish me without punishing i need to get out from this sin jesus please " >> thankyoujesus.txt
+echo "appa im sorry appa im unable to come out from this sin" >> thankyoujesus.txt
