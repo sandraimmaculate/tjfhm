fead263f4cdbbe6fa580f156e9e8372ee17c9b47
sandraimmaculate <immaculate6281@gmail.com>
2025-09-27 15:59:15 +0530

    message

--- codedeployment.sh
+++ codedeployment.sh
@@ -3,4 +3,4 @@ mkdir -p /home/jenkins/jesus
 cd /home/jenkins/jesus
 apt-get update 
 touch thankyoujesus.txt
-echo "working" >> thankyoujesus.txt
+echo "jesus im sorry i litterally not knowing it is right or wrong im unable to control my self please dont punish vinay or my mom or my dad i will try to get rid from this sin please help to get rid from this sin enaku unarthunga" >> thankyoujesus.txt
